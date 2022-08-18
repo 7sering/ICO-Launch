141b36b567d5658e6ca698e07c7d9f635d152c4d
7sering <codenow8@gmail.com>
2022-08-18 14:28:55 +0545

    ICO-Smart Contract

---
+++ harthat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x77EE3e773Dba8D438Eabd117ccA84a809dEacb68";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
